1d51af062bca86c93213d72bb56c896ad9600d79
Pshaw397 <phillip.a.shaw@googlemail.com>
2021-04-01 09:24:24 +0100

    Added day 3 version

---
+++ Day3/V3/GameStoreProject/GameStoreBusiness/CRUD_GameMethods .cs
@@ -0,0 +1,47 @@
+﻿using StoreData;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace GameStoreBusiness
+{
+    public class CRUD_GameMethods
+    {
+        public Game gameUpdate { get; set; }
+
+        public void Create(string name, string description, string coverIMG, decimal price, string publisher, int year, int month, int day)
+        {
+            Game newGame = new Game() { Name = name, Description = description, CoverImgPath = coverIMG, Price = price, Publisher = publisher, ReleaseDate = new DateTime(year, month, day) };
+            using (var db = new GameMarketContext())
+            {
+                db.Games.Add(newGame);
+                db.SaveChanges();
+            }
+        }
+
+        public void Delete(string name)
+        {
+            using (var db = new GameMarketContext())
+            {
+                gameUpdate = db.Games.Where(c => c.Name == name).FirstOrDefault();
+                db.Games.Remove(gameUpdate);
+                db.SaveChanges();
+            }
+        }
+
+        public void Update(string nameID, string name, string description, string coverIMG, decimal price, string publisher, int year, int month, int day)
+        {
+            using (var db = new GameMarketContext())
+            {
+                gameUpdate = db.Games.Where(c => c.Name == nameID).FirstOrDefault();
+                gameUpdate.Name = name;
+                gameUpdate.Description = description;
+                gameUpdate.CoverImgPath = coverIMG;
+                gameUpdate.ReleaseDate = new DateTime(year, month, day);
+                gameUpdate.Price = price;
+                gameUpdate.Publisher = publisher;
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Day3/V3/GameStoreProject/GameStoreBusiness/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StoreData;
+
+namespace StoreBusiness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new GameMarketContext())
+            {
+                db.Add(new GameDeveloper() { GameId = 1, DeveloperId = 1 });
+                db.SaveChanges();
+            }
+        }
+    }
+}
