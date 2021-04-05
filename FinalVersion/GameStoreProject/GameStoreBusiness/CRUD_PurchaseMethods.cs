0895480b1dcf99e49dda898922c71b23900dd00e
Pshaw397 <phillip.a.shaw@googlemail.com>
2021-04-05 13:21:40 +0100

    Add final project files

---
+++ FinalVersion/GameStoreProject/GameStoreBusiness/CRUD_PurchaseMethods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StoreData;
+
+namespace GameStoreBusiness
+{
+    public class CRUD_PurchaseMethods
+    {
+        public Purchase purchaseUpdate { get; set; }
+
+        public void Create(decimal userId, decimal gameID, int year, int month, int day)
+        {
+            Purchase newPurchase = new Purchase() { UserId = userId, GameId = gameID, PurchaseDate = new DateTime(year, month, day) };
+            using (var db = new GameMarketContext())
+            {
+                db.Purchases.Add(newPurchase);
+                db.SaveChanges();
+            }
+        }
+
+        public void Delete(int purchaseID)
+        {
+            using (var db = new GameMarketContext())
+            {
+                purchaseUpdate = db.Purchases.Where(c => c.PurchaseId == purchaseID).FirstOrDefault();
+                db.Purchases.Remove(purchaseUpdate);
+                db.SaveChanges();
+            }
+        }
+    }
+}
